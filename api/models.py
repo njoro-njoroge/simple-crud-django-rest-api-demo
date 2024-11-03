8f725c271a335fbacc972e7726c3c734a0b98324
njoro-njoroge <njoro.njoroge02@gmail.com>
2024-11-03 13:14:31 +0300

    Added stock field in the model

--- api/models.py
+++ api/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=11, decimal_places=2)
+    stock = models.TextField(max_length=11)
 
     """
             This method is used to display the product's
              name and price in the Django admin interface or when the object is printed.
     """
+
     def __str__(self):
-        return f'{self.name}, {self.price}'
+        return f'{self.name}, {self.price}, {self.stock}'
